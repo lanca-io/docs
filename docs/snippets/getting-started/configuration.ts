1a33f5eb1c31c4fcd57ee528bb0ea7fdfac2ab93
Oleg <olegkron.cc@gmail.com>
2024-11-12 20:08:24 +0000

    upd config

--- docs/snippets/getting-started/configuration.ts
+++ docs/snippets/getting-started/configuration.ts
@@ -4,7 +4,7 @@ const config: LancaSDKConfig = {
     integratorId: 'YOUR_INTEGRATOR_ID',
     feeTier: 1,
     chains: {
-        1: ['https://mainnet.infura.io/v3/YOUR_PROJECT_ID'],
-        4: ['https://rinkeby.infura.io/v3/YOUR_PROJECT_ID'],
+        1: ['https://rpc.ankr.com/eth', 'https://eth.llamarpc.com'],
+        137: ['https://polygon.llamarpc.com'],
     },
 }
