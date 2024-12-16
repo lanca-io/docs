ea5acc456e54e1192f0f7b83c6628dd1e8b3b9d3
Aleksei Gerasev <alexey@concero.io>
2024-12-16 15:39:59 +0300

    replace old namings

--- docs/snippets/code/getting-started/configuration.ts
+++ docs/snippets/code/getting-started/configuration.ts
@@ -1,8 +1,8 @@
 import { type LancaSDKConfig } from '@lanca/sdk'
 
 const config: LancaSDKConfig = {
-    integratorId: 'YOUR_INTEGRATOR_ID',
-    feeTier: 1,
+    integratorAddress: 'YOUR_INTEGRATOR_ADDRESS',
+    feeBps: 1,
     chains: {
         1: ['https://rpc.ankr.com/eth', 'https://eth.llamarpc.com'],
         137: ['https://polygon.llamarpc.com'],
--- docs/snippets/code/getting-started/using.ts
+++ docs/snippets/code/getting-started/using.ts
@@ -4,8 +4,8 @@ import { createWalletClient } from 'viem'
 import { polygon } from 'viem/chains'
 
 const config: LancaSDKConfig = {
-    integratorId: 'YOUR_INTEGRATOR_ID',
-    feeTier: 1,
+    integratorAddress: 'YOUR_INTEGRATOR_ADDRESS',
+    feeBps: 1,
     chains: {
         1: ['https://rpc.ankr.com/eth', 'https://eth.llamarpc.com'],
         137: ['https://polygon.llamarpc.com'],
