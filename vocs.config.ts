203fe25f53a2d24b8e54d3ae41e362c99ca21fa9
Aleksei Gerasev <alexey@concero.io>
2025-02-03 17:59:45 +0300

    format

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,51 +1,50 @@
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
-  title: 'LancaSDK Docs',
-  baseUrl: 'https://docs.lanca.io',
-  logoUrl: {
-    light: 'logo/lanca/Lanca_logo=whiteBG.svg',
-    dark: 'logo/lanca/Lanca_logo=darkBG.svg',
-  },
-  rootDir: 'docs',
-  iconUrl: 'logo/lanca/favicon.ico',
-  sidebar: [
-    {
-      text: 'Getting Started',
-      link: '/getting-started',
-    },
-    {
-      text: 'Supported Chains',
-      link: '/supported-chains',
-    },
-    {
-      text: 'Methods',
-      link: '/methods',
-    },
-    {
-      text: 'Types',
-      link: '/types',
-    },
-  ],
-  topNav: [
-    {
-      text: 'Get in touch',
-      link: 'mailto:support@lanca.io',
-    },
-  ],
-  socials: [
-    {
-      icon: 'discord',
-      link: 'https://discord.gg/lanca',
-    },
-    {
-      icon: 'github',
-      link: 'https://github.com/lanca-io',
-    },
-    {
-      icon: 'x',
-      link: 'https://x.com/lanca_io',
-    },
-
-  ],
+	title: 'LancaSDK Docs',
+	baseUrl: 'https://docs.lanca.io',
+	logoUrl: {
+		light: 'logo/lanca/Lanca_logo=whiteBG.svg',
+		dark: 'logo/lanca/Lanca_logo=darkBG.svg',
+	},
+	rootDir: 'docs',
+	iconUrl: 'logo/lanca/favicon.ico',
+	sidebar: [
+		{
+			text: 'Getting Started',
+			link: '/getting-started',
+		},
+		{
+			text: 'Supported Chains',
+			link: '/supported-chains',
+		},
+		{
+			text: 'Methods',
+			link: '/methods',
+		},
+		{
+			text: 'Types',
+			link: '/types',
+		},
+	],
+	topNav: [
+		{
+			text: 'Get in touch',
+			link: 'mailto:support@lanca.io',
+		},
+	],
+	socials: [
+		{
+			icon: 'discord',
+			link: 'https://discord.gg/lanca',
+		},
+		{
+			icon: 'github',
+			link: 'https://github.com/lanca-io',
+		},
+		{
+			icon: 'x',
+			link: 'https://x.com/lanca_io',
+		},
+	],
 })
