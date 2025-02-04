712666a8be541737e3ea68514983ae6a8cf8a409
Aleksei Gerasev <alexey@concero.io>
2025-02-04 17:40:29 +0300

    fix semi

--- snippets/code/methods/getRoute.ts
+++ snippets/code/methods/getRoute.ts
@@ -1,11 +1,11 @@
 const lancaClient = new LancaClient(config)
 const route = await lancaClient.getRoute({
-    fromChainId: '137',
-    toChainId: '8453',
-    fromToken: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', //WETH
-    toToken: '0x0555E30da8f98308EdB960aa94C0Db47230d2B9c' //WBTC
-		fromAddress: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
-		toAddress: '0x0555E30da8f98308EdB960aa94C0Db47230d2B9c',
-    amount: '100000',
-		slippageTolerance: '0.5',
+	fromChainId: '137',
+	toChainId: '8453',
+	fromToken: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', //WETH
+	toToken: '0x0555E30da8f98308EdB960aa94C0Db47230d2B9c', //WBTC
+	fromAddress: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
+	toAddress: '0x0555E30da8f98308EdB960aa94C0Db47230d2B9c',
+	amount: '100000',
+	slippageTolerance: '0.5',
 })
